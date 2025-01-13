791f804babd46b14a7f22854efd59a9ac9731622
ZumVel <64955237+ZumVel@users.noreply.github.com>
2025-01-13 19:48:40 +0300

    Added solution to 4th task of 1st stage
    Solution to a problem with several names for one task

---
+++ add_list.py
@@ -0,0 +1,16 @@
+username = input("Enter your username: ")
+title1 = input("Enter the name of your 1st task: ")
+title2 = input("Enter the name of your 2nd task: ")
+title3 = input("Enter the name of your 3rd task: ")
+content = input("Enter the content of your task: ")
+status = input("Enter the status of your task: ")
+created_date = input("Enter the created date in DD-MM format: ")
+issue_date = input("Enter the issue date in DD-MM format: ")
+titles_list = [title1, title2, title3]
+
+print(f"The username of the person creating the task: {username}")
+print(f"The titles of the task: {", ".join(titles_list)}")
+print(f"The content or description of the task: {content}")
+print(f"The current status of the task: {status}")
+print(f"The date the task was created: {created_date}")
+print(f"The date the task is issued: {issue_date}")
