b370915a4a89b12b350ac48c7ffd57e243c1b165
ZumVel <64955237+ZumVel@users.noreply.github.com>
2025-01-13 19:17:13 +0300

    Added file with solution of 2nd task of 1st stage
    Solution to the problem of removing the year from the task creation and completion date

---
+++ date_changer.py
@@ -0,0 +1,5 @@
+created_date = "13-01-2025"
+issue_date = "14-01-2025"
+
+print(f"The date the task was created: {"-".join(created_date.split("-")[:2])}")
+print(f"The date the task is issued: {"-".join(issue_date.split("-")[:2])}")
