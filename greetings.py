456d30d2b89682413512f7a5d4be85228a957785
ZumVel <64955237+ZumVel@users.noreply.github.com>
2025-01-13 18:35:00 +0300

    Added solution to the 1st task of the 1st stage
    Solution to the task of creating and outputting variables

---
+++ greetings.py
@@ -0,0 +1,15 @@
+username = "Admin"
+title = "Create a repository on github and upload the greetings.py file there"
+content = ("Create variables, assign them the appropriate values "
+           "and you will enter them using the appropriate functions. "
+           "Upload the solution file to the repository 'note_manager' on GitHub")
+status = "Created"
+created_date = "13-01-2025"
+issue_date = "14-01-2025"
+
+print(f"The username of the person creating the task: {username}")
+print(f"The title of the task: {title}")
+print(f"The content or description of the task: {content}")
+print(f"The current status of the task: {status}")
+print(f"The date the task was created: {created_date}")
+print(f"The date the task is issued: {issue_date}")
